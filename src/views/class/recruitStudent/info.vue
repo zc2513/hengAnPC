564ad9df5f2485ed73220150e70611c6f18c6f08
zc2513 <yyzc2513@163.com>
2020-08-25 19:36:43 +0800

    .

--- src/views/class/recruitStudent/info.vue
+++ src/views/class/recruitStudent/info.vue
@@ -117,7 +117,7 @@ export default {
                     { con: '删除', type: 'warning' }
                 ]
             },
-            dialogLook: true,
+            dialogLook: false,
             looks: {
                 title: '',
                 datas: {}
